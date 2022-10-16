ae351e9d5f420859cbd971444b9b4418ef89dba9
Arjun Kumar Bose (Arnob) <82363264+arjunkumarbose@users.noreply.github.com>
2022-10-16 20:54:55 +0600

    v1.3

---
+++ MID_v1.2/resources/views/inc/topnav.blade.php
@@ -0,0 +1,27 @@
+<html>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="navbar-brand" href="{{route('home')}}">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="navbar-brand" href="{{route('product')}}">Product</a>
+            </li>
+            <li class="nav-item active">
+                <a class="navbar-brand" href="{{route('team')}}">Our Teams</a>
+            </li>
+            <li class="nav-item active">
+                <a class="navbar-brand" href="{{route('about')}}">About us</a>
+            </li>
+            <li class="nav-item active">
+                <a class="navbar-brand" href="{{route('contact')}}">Contact</a>
+            </li>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</html>
